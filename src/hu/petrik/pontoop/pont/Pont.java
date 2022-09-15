806560c4cb7ad66544fbc8e795867c4077cde493
Diak <info@petrik.hu>
2022-09-15 08:45:58 +0200

    Pont osztaly - adattagok - konstruktorok

---
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -0,0 +1,18 @@
+package hu.petrik.pontoop.pont;
+
+public class Pont {
+    private int x;
+    private int y;
+
+    public Pont() {
+        x = 0;
+        y = 0;
+    }
+
+    public Pont(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+}
