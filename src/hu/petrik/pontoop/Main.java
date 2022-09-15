6fd6aefd4eff9a75bd8fe36b38066f795f94ddf4
Diak <info@petrik.hu>
2022-09-15 09:18:39 +0200

    10 véletlen pont létrehozása és kiirasa

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -16,6 +16,17 @@ public class Main {
         System.out.println(p3);
         System.out.println(p4);
 
+
+        Pont[] pontok = new Pont[10];
+        for (int i = 0; i < pontok.length; i++) {
+            pontok[i] = new Pont(100);
+        }
+        for (Pont p: pontok)
+        {
+            System.out.println(p);
+
+        }
+
     }
 
 
