9ab70f0a553dcc5ba8d70cc7b7cc24cd3ba83e97
Diak <info@petrik.hu>
2022-09-15 09:12:12 +0200

    Egy parameteres konstruktor létrehozása

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -9,10 +9,12 @@ public class Main {
         Pont p1 = new Pont();
         Pont p2 = new Pont(0,0);
         Pont p3 = new Pont(3,4);
+        Pont p4 = new Pont(100);
 
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+        System.out.println(p4);
 
     }
 
